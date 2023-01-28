e67a124de0d1ac8241d4b81a713849b6472e80a6
Yukirachi <110596236+Yukirachi@users.noreply.github.com>
2023-01-28 15:23:42 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    tablet: {smooth: true},
+    smartphone: {smooth: true},
+});
+
