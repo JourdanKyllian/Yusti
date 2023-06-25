88b7026efd42cedc7d7bb426350a3de9f515839d
KyllianDev <110596236+JourdanKyllian@users.noreply.github.com>
2023-06-25 12:59:15 +0200

    Delete assets directory

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    tablet: {smooth: true},
-    smartphone: {smooth: true},
-});
-
